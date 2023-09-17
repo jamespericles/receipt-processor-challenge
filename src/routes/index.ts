d2d91aa3593d446585e599c082b725e3ee45e300
James Pericles <jamesspericles@gmail.com>
2023-09-16 20:35:30 -0500

    init go implementation

--- src/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express'
-import { processReceipt, getPoints } from '../controller'
-
-const router: Router = Router()
-
-router.post('/process', processReceipt)
-router.get('/:id/points', getPoints)
-
-export default router
